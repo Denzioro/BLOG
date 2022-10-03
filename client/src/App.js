378642d0f84775f74144e089d0dc0214d2cfec77
Denny <den.shel@inbox.ru>
2022-10-03 14:32:22 +0300

    r2.2 auth-registration

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,29 @@
 import Container from '@mui/material/Container';
 import { Header } from './components';
-import { Home } from './pages/Home';
+import { Home, FullPost, AddPost, Login, Registration } from './pages';
+import { Routes, Route } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import React from 'react';
+import { fetchAuthMe, selectIsAuth } from './redux/slices/auth';
+
 function App() {
+  const isAuth = useSelector(selectIsAuth);
+  const dispatch = useDispatch();
+
+  React.useEffect(() => {
+    dispatch(fetchAuthMe());
+  }, [dispatch]);
   return (
     <>
       <Header />
       <Container maxWidth="lg">
-        <Home />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/posts/:id" element={<FullPost />} />
+          <Route path="/add-post" element={<AddPost />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Registration />} />
+        </Routes>
       </Container>
     </>
   );
--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import multer from 'multer';
+import cors from 'cors';
 import mongoose from 'mongoose';
 
 import {
@@ -32,6 +33,7 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage });
 app.use(express.json());
+app.use(cors());
 app.use('/uploads', express.static('uploads'));
 
 app.get('/', (req, res) => {
@@ -58,7 +60,10 @@ app.post('/upload', checkAuth, upload.single('image'), (req, res) => {
   });
 });
 
+app.get('/tags', PostControler.getLastTags);
+
 app.get('/posts', PostControler.getAllPosts);
+app.get('/posts/tags', PostControler.getLastTags);
 app.get('/posts/:id', PostControler.getOnePost);
 app.post(
   '/posts',
